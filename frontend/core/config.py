9534267d60718b9c22046b484ca85fdfd037aba0
rarm <rarm.jet@gmail.com>
2020-10-05 14:05:19 +1100

    update default url

--- frontend/core/config.py
+++ frontend/core/config.py
@@ -3,8 +3,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Quotegen Frontend"
-    url_quote_backend: str = "http://backend:8000"
-    image_backend_api: str = "http://backend:8000/quote/image"
+    url_quote_backend: str = "http://backend:8080"
+    image_backend_api: str = "http://backend:8080/quote/image"
     log_level: str = "INFO"
 
     class Config:
